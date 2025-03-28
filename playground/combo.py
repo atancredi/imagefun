488fd18007b9eaba68bfd91857057f0d3ee355e1
atancredi <alessandro.tancredi@aitechnologies.it>
2025-03-28 14:10:49 +0100

    some updates, watermark script

--- playground/combo.py
+++ playground/combo.py
@@ -2,78 +2,89 @@ from PIL import Image
 import numpy as np
 from tqdm import  tqdm
 
-def saturation(image: Image, amount: int):
-    width = image.width
-    height = image.height
-    image2 = Image.new("RGB", (width, height))
-    newpixel = [0, 0, 0]
-    for x in tqdm(range(width), desc="Saturation"):
-        for y in tqdm(range(height),leave=False):
-            oldpixel = image.getpixel((x, y))
-            average = sum(oldpixel) // 3
-            for i in range(3):
-                newpixel[i] = int(
-                    max(
-                        min((oldpixel[i] - average) * amount + average, 
-                            255),
-                            0))
-                image2.putpixel((x, y), tuple(newpixel))
-    return image2
+from imagefun import Imagefun
 
-def invert_channels(image: Image):
-    width = image.width
-    height = image.height
-    image3 = Image.new("RGB", (width, height))
-    for x in tqdm(range(width), desc="Inverting channels"):
-        for y in tqdm(range(height),leave=False):
-            oldpixel = image.getpixel((x, y))
-            newpixel = (oldpixel[2], oldpixel[0], oldpixel[1]) 
-            image3.putpixel((x, y), newpixel)
-    return image3
+# TODO just for fun, use a class decorator to avoid declaring __init__ (like dataclasses i guess)
+class ComboFilters(Imagefun):
+    
+	def __init__(self, filename, properties):
+		super().__init__(filename, properties)
 
+	def saturation(self, amount: int):
+		width = self.image.width
+		height = self.image.height
+		image2 = Image.new("RGB", (width, height))
+		newpixel = [0, 0, 0]
+		for x in tqdm(range(width), desc="Saturation"):
+			for y in tqdm(range(height),leave=False):
+				oldpixel = self.image.getpixel((x, y))
+				average = sum(oldpixel) // 3
+				for i in range(3):
+					newpixel[i] = int(
+						max(
+							min((oldpixel[i] - average) * amount + average, 
+								255),
+								0))
+					image2.putpixel((x, y), tuple(newpixel))
+		return self
 
-def rebalance_levels(image: Image):
-    width = image.width
-    height = image.height
-    image4 = Image.new("RGB", (width, height))
-    for x in tqdm(range(width), desc="Rebalancing levels"):
-        for y in tqdm(range(height),leave=False):
-            oldpixel = image.getpixel((x, y))
-            newpixel = (int(min(oldpixel[0] * 1.9, 255)),
-        		oldpixel[1],
-            	int(oldpixel[2] * 0.1))
-            image4.putpixel((x, y), newpixel)
-    return image4
+	def invert_channels(self):
+		width = self.image.width
+		height = self.image.height
+		image3 = Image.new("RGB", (width, height))
+		for x in tqdm(range(width), desc="Inverting channels"):
+			for y in tqdm(range(height),leave=False):
+				oldpixel = self.image.getpixel((x, y))
+				newpixel = (oldpixel[2], oldpixel[0], oldpixel[1]) 
+				image3.putpixel((x, y), newpixel)
+		return self
 
-def bitwise_or(image: Image):
-    image_np = np.array(image, dtype=np.uint8)
-    for i in tqdm(range(1, image_np.shape[0] - 1), desc="Bitwise OR"):
-        image_np[i:-1:5, 1:-1:5] |= 0xE0  # Apply bitwise OR only on valid pixels
-        image5 = Image.fromarray(image_np, "RGB")
-    return image5
+	def rebalance_levels(self):
+		width = self.image.width
+		height = self.image.height
+		image4 = Image.new("RGB", (width, height))
+		for x in tqdm(range(width), desc="Rebalancing levels"):
+			for y in tqdm(range(height),leave=False):
+				oldpixel = self.image.getpixel((x, y))
+				newpixel = (int(min(oldpixel[0] * 1.9, 255)),
+					oldpixel[1],
+					int(oldpixel[2] * 0.1))
+				image4.putpixel((x, y), newpixel)
+		self.image = image4
+		return self
 
-def bitwise_and(image: Image):
-    image_np = np.array(image, dtype=np.uint8)
-    for i in tqdm(range(1, image_np.shape[0] - 1), desc="Bitwise AND"):
-        image_np[i:-1, 1:-1] &= 0xE0  # Apply bitwise OR only on valid pixels
-        image5 = Image.fromarray(image_np, "RGB")
-    return image5
+	def bitwise_or(self):
+		image_np = np.array(self.image, dtype=np.uint8)
+		for i in tqdm(range(1, image_np.shape[0] - 1), desc="Bitwise OR"):
+			image_np[i:-1:5, 1:-1:5] |= 0xE0  # Apply bitwise OR only on valid pixels
+			
+			# self.image = Image.fromarray(image_np, "RGB") # NOSONAR mi sa che era sbagliato
+		
+		self.image = Image.fromarray(image_np, "RGB")
+		return self
+
+	def bitwise_and(self):
+		image_np = np.array(self.image, dtype=np.uint8)
+		for i in tqdm(range(1, image_np.shape[0] - 1), desc="Bitwise AND"):
+			image_np[i:-1, 1:-1] &= 0xE0  # Apply bitwise OR only on valid pixels
+		
+			# image5 = Image.fromarray(image_np, "RGB") # NOSONAR mi sa che era sbagliato
+
+		self.image = Image.fromarray(image_np, "RGB")
+		return self
+
 
 import fire
 def hello(filename):
-    image=Image.open(filename)
-    height = int(image.height * 1440 / image.width)
-    width = 1440
-    image = image.resize((width, height))
+
+	img = ComboFilters(filename)
     
-    image2 = saturation(image, 20)
-    image3 = invert_channels(image2)
-    image4 = rebalance_levels(image3)
-    image5 = bitwise_and(image4)
-    # image5 = bitwise_or(image4)
-    _ = filename.split(".")
-    f =  _[0] + "_" + 'edit' + "." + _[1]
-    image5.save(f)
+	img.saturation(20)\
+	.invert_channels()\
+	.rebalance_levels()\
+	.bitwise_and()\
+	.save()
+	# .bitwise_or()\
     
 
 if __name__ == '__main__':
